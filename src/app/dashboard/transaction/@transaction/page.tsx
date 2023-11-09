339d9626caae8b78147f9156878b1248422984e6
20chan <2@0chan.dev>
2023-11-09 17:12:14 +0900

    preload

--- src/app/dashboard/transaction/@transaction/page.tsx
+++ src/app/dashboard/transaction/@transaction/page.tsx
@@ -5,9 +5,17 @@ import { NextProps } from '@/lib/NextProps';
 import { CategoryParams } from '@/lib/params';
 import { PaginationParams } from '@/lib/params/PaginationParams';
 
-export default async function TransactionListPage(props: NextProps) {
+export const preload = async (props: NextProps) => {
   const transactions = await fetchTransactionsWithCategories();
   const categories = await fetchCategoriesWrapped();
+  return {
+    transactions,
+    categories,
+  };
+}
+
+export default async function TransactionListPage(props: NextProps) {
+  const { transactions, categories } = await preload(props);
 
   const context = {
     props,
